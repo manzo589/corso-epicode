f8cd54219c96a5aa4bd3cdfda96c84022a887449
Fabio Manzo <manzabio@gmail.com>
2026-04-28 01:08:21 +0200

    prove python

---
+++ secondo file.py
@@ -0,0 +1,16 @@
+#stampa del nome
+x="fabio"
+print ("ciao" ,x)
+
+#input età e raddoppiala
+eta=int(input("quanti anni hai?"))
+print("tra" ,eta ,"anni avrai", eta*2 , "anni") 
+
+#somma 2 numeri inseriti da utente
+primo=int(input("digita il primo numero "))
+secondo=int(input("digita il secondo numero "))
+print("la somma di" ,primo ,"+" ,secondo ,"è" ,primo+secondo)
+
+#prova if else
+if primo+secondo > eta: print("maggiore di eta")
+else: print("minore di eta")
