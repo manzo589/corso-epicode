ae7dc03e4a03a444c20b85542b66d259038a0837
Fabio Manzo <manzabio@gmail.com>
2026-06-12 00:05:50 +0200

    Create progetto_1.py

---
+++ progetto_1.py
@@ -0,0 +1,26 @@
+#------------------- Parte 1 -------------------
+titolo = "il signore degli anelli"
+copie = 5
+prezzo = 9.95
+disponibile = True
+
+print("Titolo del libro:", titolo)
+print("Copie disponibili:", copie)
+print("Prezzo medio:", prezzo)
+print("Disponibile:", disponibile)
+
+
+#------------------- Parte 2 -------------------
+lista_libri = ["il signore degli anelli" , "cars" , "zootropolis" , "la cariaca dei 101" , "oceania"]
+
+dizionario_libri = \
+{"il signore degli anelli" : 5 },
+{"cars" : 4 },
+{"zootropolis" : 3 },
+{"la cariaca dei 101" : 2 },
+{"oceania" : 1 }
+
+utenti = {"fabio" , "federica" , "vittoria" , "achille" , "luna" }
+
+
+#------------------- Parte 3 -------------------
